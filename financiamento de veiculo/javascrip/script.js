cb3b5d5522a342564fd2b65110f3c47fe819b8b4
barroso2ctn <123496253+barroso2ctn@users.noreply.github.com>
2025-06-06 22:37:33 -0300

    Add files via upload

---
+++ financiamento de veiculo/javascrip/script.js
@@ -0,0 +1,9 @@
+const amount = document.querySelector("#amount") // input da quantia total
+const institution = document.querySelector("#institution") // select com as instituições
+const period = document.querySelector("#period") //select com as quantidades de prazo
+const calculate = document.querySelector("#calculate") //botão para executar a função o processarDados()
+
+const installmentValue = document.querySelector(".installment-value") // campo para receber o valor das mensalidades
+const totalValue = document.querySelector(".total-value") // campo para receber o valor final
+const paymentPeriod = document.querySelector(".payment-period") // campo das quantidades de prestações
+const btnCalculateAgain = document.querySelector(".btn-calculate-again") //botão para retornar a pagina inicial
